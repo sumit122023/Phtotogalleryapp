f50617dbe6e5a0ef5dcecfeabf306c258972d16e
sumit122023 <sumit@bluebash.co>
2023-09-01 16:50:40 +0530

    edit user_id in it

--- db/migrate/20230823044720_add_published_to_albums.rb
+++ db/migrate/20230823044720_add_published_to_albums.rb
@@ -1,6 +1,6 @@
 class AddPublishedToAlbums < ActiveRecord::Migration[7.0]
   def change
     add_column :albums, :published, :boolean
-    add_reference :albums, :user,  foreign_key: true
+    # add_reference :albums, :user,  foreign_key: true
   end
 end
